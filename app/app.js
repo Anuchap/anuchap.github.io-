86cda1b12f3034f0a3d0ff486b08f26a5571071c
Anuchap <mr.izedtea@gmail.com>
2015-07-09 10:09:12 +0700

    SPA

---
+++ app/app.js
@@ -0,0 +1,26 @@
+var app = angular.module('MyApp', ['ngRoute', 'ui.bootstrap']);
+
+app.config(function ($routeProvider) {
+
+    $routeProvider.when('/', {
+        templateUrl: '/app/views/home.html'
+    });
+
+    $routeProvider.when('/datepicker', {
+        controller: 'DatePickerCtrl',
+        templateUrl: '/app/views/datepicker.html'
+    });
+
+    $routeProvider.otherwise({
+        redirectTo: '/'
+    });
+});
+
+app.controller('DatePickerCtrl', function ($scope) {
+
+    $scope.open = function ($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+        $scope.opened = true;
+    };
+});
